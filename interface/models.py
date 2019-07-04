a2c9f47048730e2d2d73755e572be6ad77c8908b
Russell Hofvendahl <rhofvendahl@gmail.com>
2019-07-03 18:01:42 -0700

    add dja backend framework

---
+++ interface/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+#
+#
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+
+class DeviceSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start = models.DateTimeField(auto_now_add=True)
+    stop = models.DateTimeField(blank=True)
+
+class Signal(models.Model):
+    device_session = models.ForeignKey(DeviceSession, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
